271f6add68903145dff7e0832f91381713207f28
Akira Sugiura <urasandesu@gmail.com>
2010-08-28 18:33:53 +0900

    Add windows enumeration sample. Be careful, EnumWindows enumerates the windows in top-down Z-order.

---
+++ Urasandesu.NTroll.EnumerateWindowsSample/Window1.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Urasandesu.NTroll.PInvoke;
+using System.Diagnostics;
+using Urasandesu.NAnonym.Linq;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Urasandesu.NTroll.PInvoke.Structures;
+using Urasandesu.NTroll.PInvoke.Enums;
+using System.Drawing;
+using System.Windows.Interop;
+using System.Collections.ObjectModel;
+
+namespace Urasandesu.NTroll.EnumerateWindowsSample
+{
+    /// <summary>
+    /// Window1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            IntPtr window1HWnd = ((HwndSource)HwndSource.FromVisual(this)).Handle;
+            //var zOrder = new LinkedList<IntPtr>();
+            //var zOrder = new List<IntPtr>();
+
+            bool success = 
+            User32.EnumWindows(
+                (hWnd, lParam) =>
+                {
+                    if (!User32.IsWindowVisible(hWnd)) return true;
+
+                    // MEMO: ／(^o^)＼ http://stackoverflow.com/questions/295996/is-the-order-in-which-handles-are-returned-by-enumwindows-meaningful
+                    string title = GetWindowTitle(hWnd);
+                    if (!string.IsNullOrEmpty(title))
+                    {
+                        Console.WriteLine("Window: {0}", title);
+                    }
+
+                    //IntPtr hWndPrev = IntPtr.Zero;
+                    //var hWndNode = default(LinkedListNode<IntPtr>);
+                    //var hWndPrevNode = default(LinkedListNode<IntPtr>);
+                    //if ((hWndPrev = User32.GetWindow(hWnd, GetWindow_Cmd.GW_HWNDPREV)) != IntPtr.Zero)
+                    //{
+                    //    if (zOrder.Count == 0)
+                    //    {
+                    //        hWndNode = zOrder.AddFirst(hWnd);
+                    //        zOrder.AddAfter(hWndNode, hWndPrev);
+                    //    }
+                    //    else
+                    //    {
+                    //        if ((hWndNode = zOrder.Find(hWnd)) != null)
+                    //        {
+                    //            if ((hWndPrevNode = zOrder.FindLast(hWndPrev)) != null)
+                    //            {
+                    //                zOrder.Remove(hWndPrevNode);
+                    //            }
+                    //            zOrder.AddAfter(hWndNode, hWndPrev);
+                    //        }
+                    //        else if ((hWndPrevNode = zOrder.Find(hWndPrev)) != null)
+                    //        {
+                    //            if ((hWndNode = zOrder.FindLast(hWnd)) != null)
+                    //            {
+                    //                zOrder.Remove(hWndNode);
+                    //            }
+                    //            zOrder.AddBefore(hWndPrevNode, hWnd);
+                    //        }
+                    //        else
+                    //        {
+                    //            hWndNode = zOrder.AddLast(hWnd);
+                    //            zOrder.AddAfter(hWndNode, hWndPrev);
+                    //        }
+                    //    }
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine("Expected error at GetWindow. {0}", new Win32Exception(Marshal.GetLastWin32Error()).Message);
+                    //}
+
+                    return true;
+                }, 
+                IntPtr.Zero);
+
+            if (!success)
+            {
+                Console.WriteLine("Expected error at EnumThreadWindows.");
+            }
+
+            //Console.WriteLine("Z-Order: ");
+            //zOrder.ForEach(
+            //hWnd =>
+            //{
+            //    string title = GetWindowTitle(hWnd);
+            //    if (!string.IsNullOrEmpty(title))
+            //    {
+            //        Console.WriteLine("Window: {0}", title);
+            //    }
+            //});
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            new Window2() { Owner = this, Title = "Hoge" }.Show();
+        }
+
+        string GetWindowTitle(IntPtr hWnd)
+        {
+            int length = User32.GetWindowTextLength(hWnd);
+            if (0 < length)
+            {
+                var lpString = new StringBuilder(length + 1);
+                if (0 < User32.GetWindowText(hWnd, lpString, lpString.Capacity))
+                {
+                    return lpString.ToString();
+                }
+                else
+                {
+                    //Console.WriteLine("Expected error at GetWindowText. {0}", new Win32Exception(Marshal.GetLastWin32Error()).Message);
+                    return null;
+                }
+            }
+            else
+            {
+                //Console.WriteLine("Expected error at GetWindowTextLength. {0}", new Win32Exception(Marshal.GetLastWin32Error()).Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ Urasandesu.NTroll.PInvoke/Class1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using Urasandesu.NAnonym;
+using Urasandesu.NTroll.PInvoke.Structures;
+using Urasandesu.NTroll.PInvoke.Delegates;
+using Urasandesu.NTroll.PInvoke.Enums;
+using System.Drawing;
+
+namespace Urasandesu.NTroll.PInvoke.Enums
+{
+    public enum GetWindow_Cmd : uint
+    {
+        GW_HWNDFIRST = 0,
+        GW_HWNDLAST = 1,
+        GW_HWNDNEXT = 2,
+        GW_HWNDPREV = 3,
+        GW_OWNER = 4,
+        GW_CHILD = 5,
+        GW_ENABLEDPOPUP = 6
+    }
+
+
+
+    [Flags]
+    public enum WindowStyles : uint
+    {
+        WS_OVERLAPPED = 0x00000000,
+        WS_POPUP = 0x80000000,
+        WS_CHILD = 0x40000000,
+        WS_MINIMIZE = 0x20000000,
+        WS_VISIBLE = 0x10000000,
+        WS_DISABLED = 0x08000000,
+        WS_CLIPSIBLINGS = 0x04000000,
+        WS_CLIPCHILDREN = 0x02000000,
+        WS_MAXIMIZE = 0x01000000,
+        WS_BORDER = 0x00800000,
+        WS_DLGFRAME = 0x00400000,
+        WS_VSCROLL = 0x00200000,
+        WS_HSCROLL = 0x00100000,
+        WS_SYSMENU = 0x00080000,
+        WS_THICKFRAME = 0x00040000,
+        WS_GROUP = 0x00020000,
+        WS_TABSTOP = 0x00010000,
+        WS_MINIMIZEBOX = 0x00020000,
+        WS_MAXIMIZEBOX = 0x00010000,
+        WS_CAPTION = WS_BORDER | WS_DLGFRAME,
+        WS_TILED = WS_OVERLAPPED,
+        WS_ICONIC = WS_MINIMIZE,
+        WS_SIZEBOX = WS_THICKFRAME,
+        WS_TILEDWINDOW = WS_OVERLAPPEDWINDOW,
+        WS_OVERLAPPEDWINDOW = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
+        WS_POPUPWINDOW = WS_POPUP | WS_BORDER | WS_SYSMENU,
+        WS_CHILDWINDOW = WS_CHILD,
+        //Extended Window Styles
+        WS_EX_DLGMODALFRAME = 0x00000001,
+        WS_EX_NOPARENTNOTIFY = 0x00000004,
+        WS_EX_TOPMOST = 0x00000008,
+        WS_EX_ACCEPTFILES = 0x00000010,
+        WS_EX_TRANSPARENT = 0x00000020,
+        //#if(WINVER >= 0x0400)
+        WS_EX_MDICHILD = 0x00000040,
+        WS_EX_TOOLWINDOW = 0x00000080,
+        WS_EX_WINDOWEDGE = 0x00000100,
+        WS_EX_CLIENTEDGE = 0x00000200,
+        WS_EX_CONTEXTHELP = 0x00000400,
+        WS_EX_RIGHT = 0x00001000,
+        WS_EX_LEFT = 0x00000000,
+        WS_EX_RTLREADING = 0x00002000,
+        WS_EX_LTRREADING = 0x00000000,
+        WS_EX_LEFTSCROLLBAR = 0x00004000,
+        WS_EX_RIGHTSCROLLBAR = 0x00000000,
+        WS_EX_CONTROLPARENT = 0x00010000,
+        WS_EX_STATICEDGE = 0x00020000,
+        WS_EX_APPWINDOW = 0x00040000,
+        WS_EX_OVERLAPPEDWINDOW = (WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE),
+        WS_EX_PALETTEWINDOW = (WS_EX_WINDOWEDGE | WS_EX_TOOLWINDOW | WS_EX_TOPMOST),
+        //#endif /* WINVER >= 0x0400 */
+        //#if(_WIN32_WINNT >= 0x0500)
+        WS_EX_LAYERED = 0x00080000,
+        //#endif /* _WIN32_WINNT >= 0x0500 */
+        //#if(WINVER >= 0x0500)
+        WS_EX_NOINHERITLAYOUT = 0x00100000,
+        WS_EX_LAYOUTRTL = 0x00400000,
+        //#endif /* WINVER >= 0x0500 */
+        //#if(_WIN32_WINNT >= 0x0500)
+        WS_EX_COMPOSITED = 0x02000000,
+        WS_EX_NOACTIVATE = 0x08000000
+        //#endif /* _WIN32_WINNT >= 0x0500 */
+    }
+
+    [Flags]
+    public enum WindowExStyles : uint
+    {
+        WS_EX_ACCEPTFILES = 0x00000010,
+        WS_EX_APPWINDOW = 0x00040000,
+        WS_EX_CLIENTEDGE = 0x00000200,
+        WS_EX_COMPOSITED = 0x02000000,
+        WS_EX_CONTEXTHELP = 0x00000400,
+        WS_EX_CONTROLPARENT = 0x00010000,
+        WS_EX_DLGMODALFRAME = 0x00000001,
+        WS_EX_LAYERED = 0x00080000,
+        WS_EX_LAYOUTRTL = 0x00400000,
+        WS_EX_LEFT = 0x00000000,
+        WS_EX_LEFTSCROLLBAR = 0x00004000,
+        WS_EX_LTRREADING = 0x00000000,
+        WS_EX_MDICHILD = 0x00000040,
+        WS_EX_NOACTIVATE = 0x08000000,
+        WS_EX_NOINHERITLAYOUT = 0x00100000,
+        WS_EX_NOPARENTNOTIFY = 0x00000004,
+        WS_EX_OVERLAPPEDWINDOW = WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE,
+        WS_EX_PALETTEWINDOW = WS_EX_WINDOWEDGE | WS_EX_TOOLWINDOW | WS_EX_TOPMOST,
+        WS_EX_RIGHT = 0x00001000,
+        WS_EX_RIGHTSCROLLBAR = 0x00000000,
+        WS_EX_RTLREADING = 0x00002000,
+        WS_EX_STATICEDGE = 0x00020000,
+        WS_EX_TOOLWINDOW = 0x00000080,
+        WS_EX_TOPMOST = 0x00000008,
+        WS_EX_TRANSPARENT = 0x00000020,
+        WS_EX_WINDOWEDGE = 0x00000100
+    }
+
+    public enum WindowStatus : uint
+    {
+        Otherwise = 0,
+        WS_ACTIVECAPTION = 0x0001
+    }
+}
+
+namespace Urasandesu.NTroll.PInvoke.Structures
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        int _left;
+        int _top;
+        int _right;
+        int _bottom;
+
+        public RECT(int left, int top, int right, int bottom)
+        {
+            _left = left;
+            _top = top;
+            _right = right;
+            _bottom = bottom;
+        }
+
+        public int X
+        {
+            get { return Left; }
+            set { Left = value; }
+        }
+        public int Y
+        {
+            get { return Top; }
+            set { Top = value; }
+        }
+        public int Left
+        {
+            get { return _left; }
+            set { _left = value; }
+        }
+        public int Top
+        {
+            get { return _top; }
+            set { _top = value; }
+        }
+        public int Right
+        {
+            get { return _right; }
+            set { _right = value; }
+        }
+        public int Bottom
+        {
+            get { return _bottom; }
+            set { _bottom = value; }
+        }
+        public int Height
+        {
+            get { return Bottom - Top; }
+            set { Bottom = value - Top; }
+        }
+        public int Width
+        {
+            get { return Right - Left; }
+            set { Right = value + Left; }
+        }
+
+        public static implicit operator Rectangle(RECT rect)
+        {
+            return new Rectangle(rect.X, rect.Y, rect.Width, rect.Height);
+        }
+
+        public static implicit operator Region(RECT rect)
+        {
+            return new Region(rect);
+        }
+
+        public override string ToString()
+        {
+            return "{Left: " + Left + "; " + "Top: " + Top + "; Right: " + Right + "; Bottom: " + Bottom + "}";
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WINDOWINFO
+    {
+        public uint cbSize;
+        public RECT rcWindow;
+        public RECT rcClient;
+        public uint dwStyle;
+        public uint dwExStyle;
+        public uint dwWindowStatus;
+        public uint cxWindowBorders;
+        public uint cyWindowBorders;
+        public ushort atomWindowType;
+        public ushort wCreatorVersion;
+
+        public WINDOWINFO(bool initialize)
+            : this()
+        {
+            Required.MustBeSet(initialize, true, () => initialize);
+            cbSize = (UInt32)(Marshal.SizeOf(typeof(WINDOWINFO)));
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendFormat("cbSize: {0}, ", cbSize);
+            stringBuilder.AppendFormat("rcWindow: {0}, ", rcWindow);
+            stringBuilder.AppendFormat("rcClient: {0}, ", rcClient);
+            stringBuilder.AppendFormat("dwStyle: {0}, ", (WindowStyles)dwStyle);
+            stringBuilder.AppendFormat("dwExStyle: {0}, ", (WindowExStyles)dwExStyle);
+            stringBuilder.AppendFormat("dwWindowStatus: {0}, ", (WindowStatus)dwWindowStatus);
+            stringBuilder.AppendFormat("cxWindowBorders: {0}, ", cxWindowBorders);
+            stringBuilder.AppendFormat("cyWindowBorders: {0}, ", cyWindowBorders);
+            stringBuilder.AppendFormat("atomWindowType: {0}, ", atomWindowType);
+            stringBuilder.AppendFormat("wCreatorVersion: {0}", wCreatorVersion);
+            return stringBuilder.ToString();
+        }
+    }
+}
+
+namespace Urasandesu.NTroll.PInvoke.Delegates
+{
+    public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+    public delegate bool EnumThreadDelegate(IntPtr hWnd, IntPtr lParam);
+}
+
+namespace Urasandesu.NTroll.PInvoke
+{
+
+    public static class User32
+    {
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindow(IntPtr hWnd, GetWindow_Cmd uCmd);        
+        
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetWindowInfo(IntPtr hWnd, ref WINDOWINFO pwi);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);        
+        
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+        
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);        
+        
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumThreadWindows(uint dwThreadId, EnumThreadDelegate lpfn, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+    }
+}
