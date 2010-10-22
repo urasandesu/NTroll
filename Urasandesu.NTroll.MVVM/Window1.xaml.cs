d2c3b03cd983b944a2c6da8e88439ca2287b7acb
Akira Sugiura <urasandesu@gmail.com>
2010-10-22 22:33:16 +0900

    Try whether MarkupExtension can return UIElement(and it was possible).

---
+++ Urasandesu.NTroll.MVVM/Window1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Markup;
+
+namespace Urasandesu.NTroll.MVVM
+{
+    /// <summary>
+    /// Window1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class PluginProviderExtension : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var button = new Button();
+            button.Content = "押してね";
+            button.Click += (sender, e) => MessageBox.Show("Hello, MVVM!!");
+            return button;
+        }
+    }
+}
