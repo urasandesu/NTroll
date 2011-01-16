75d419c95099c04ed4751b2cd88f7848610936a8
Akira Sugiura <urasandesu@gmail.com>
2011-01-16 18:19:28 +0900

    Modify sample using NAnonym because I changed the internal DSL declaration.

--- Urasandesu.NTroll.AutoGenerationHolic.NAnonymSample/MethodInjector.cs
+++ Urasandesu.NTroll.AutoGenerationHolic.NAnonymSample/MethodInjector.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using Urasandesu.NTroll.AutoGenerationHolic.Helpers;
-using Urasandesu.NTroll.AutoGenerationHolic.Helpers.Mixins.System;
 using System.Reflection;
 using System.Reflection.Emit;
 using Urasandesu.NAnonym.Mixins.System.Reflection.Emit;
+using Urasandesu.NTroll.AutoGenerationHolic.Helpers;
+using Urasandesu.NTroll.AutoGenerationHolic.Helpers.Mixins.System;
+using Urasandesu.NAnonym.Mixins.Urasandesu.NAnonym.ILTools;
 
 namespace Urasandesu.NTroll.AutoGenerationHolic.NAnonymSample
 {
@@ -41,34 +40,38 @@ namespace Urasandesu.NTroll.AutoGenerationHolic.NAnonymSample
             method.ExpressBody(
             gen =>
             {
+                var equals = default(bool);
                 var that = default(T);
-                gen.Eval(_ => _.If(_.Alloc(that).As(_.LdArg(1) as T) == null));
+                gen.Eval(_ => _.If(_.Alloc(that).As(_.LdArg<T>(1) as T) == null));
                 {
-                    gen.Eval(_ => _.Return(false));
+                    gen.Eval(_ => _.Alloc(equals).As(false));
+                }
+                gen.Eval(_ => _.Else());
+                {
+                    var _this = default(T);
+                    gen.Eval(_ => _.Alloc(_this).As(_.LdArg<T>(0)));
+                    if (properties.Length == 0)
+                    {
+                        gen.Eval(_ => _.Alloc(equals).As(_this.Equals(that)));
+                    }
+                    else if (1 <= properties.Length)
+                    {
+                        var x = default(object);
+                        gen.Eval(_ => _.Alloc(x).As(properties[0].GetValue(_this, null)));
+                        var y = default(object);
+                        gen.Eval(_ => _.Alloc(y).As(properties[0].GetValue(that, null)));
+                        gen.Eval(_ => _.Alloc(equals).As(x.Equals(y)));
+                        for (int i = 1; i < properties.Length; i++)
+                        {
+                            gen.Eval(_ => _.Alloc(x).As(properties[i].GetValue(_this, null)));
+                            gen.Eval(_ => _.Alloc(y).As(properties[i].GetValue(that, null)));
+                            gen.Eval(_ => _.Alloc(equals).As(equals && x.Equals(y)));
+                        }
+                    }
                 }
                 gen.Eval(_ => _.EndIf());
 
-                if (properties.Length == 0)
-                {
-                    gen.Eval(_ => _.Return(_.LdArg(0).Equals(that)));
-                }
-                else if (properties.Length == 1)
-                {
-                    var gm = properties[0].GetGetMethod();
-                    gen.Eval(_ => _.Return(_.AreEqual(_.Invoke(_.LdArg(0), _.X(gm)), _.Invoke(that, _.X(gm)))));
-                }
-                else if (2 <= properties.Length)
-                {
-                    var gm = properties[0].GetGetMethod();
-                    var equals = default(bool);
-                    gen.Eval(_ => _.Alloc(equals).As(_.AreEqual(_.Invoke(_.LdArg(0), _.X(gm)), _.Invoke(that, _.X(gm)))));
-                    for (int i = 1; i < properties.Length; i++)
-                    {
-                        gm = properties[i].GetGetMethod();
-                        gen.Eval(_ => _.Alloc(equals).As(equals && _.AreEqual(_.Invoke(_.LdArg(0), _.X(gm)), _.Invoke(that, _.X(gm)))));
-                    }
-                    gen.Eval(_ => _.Return(equals));
-                }
+                gen.Eval(_ => _.Return(equals));
             });
             return (Func<T, object, bool>)method.CreateDelegate(typeof(Func<T, object, bool>));
         }
@@ -79,52 +82,41 @@ namespace Urasandesu.NTroll.AutoGenerationHolic.NAnonymSample
             method.ExpressBody(
             gen =>
             {
+                var hashCode = default(int);
+                var _this = default(T);
+                gen.Eval(_ => _.Alloc(_this).As(_.LdArg<T>(0)));
                 if (properties.Length == 0)
                 {
-                    gen.Eval(_ => _.Return(_.LdArg(0).GetHashCode()));
+                    gen.Eval(_ => _.Alloc(hashCode).As(_this.GetHashCode()));
                 }
-                else if (properties.Length == 1)
+                else if (1 < properties.Length)
                 {
-                    var gm = properties[0].GetGetMethod();
+                    var obj = default(object);
+                    gen.Eval(_ => _.Alloc(obj).As(properties[0].GetValue(_this, null)));
                     if (properties[0].PropertyType.IsValueType)
                     {
-                        gen.Eval(_ => _.Return(_.Invoke(_.LdArg(0), _.X(gm)).GetHashCode()));
+                        gen.Eval(_ => _.Alloc(hashCode).As(obj.GetHashCode()));
                     }
                     else
                     {
-                        gen.Eval(_ => _.Return(_.Invoke(_.LdArg(0), _.X(gm)) == null ? 0 : _.Invoke(_.LdArg(0), _.X(gm)).GetHashCode()));
-                    }
-                }
-                else if (2 <= properties.Length)
-                {
-                    var hashCode = default(int);
-                    var gm = properties[0].GetGetMethod();
-                    if (properties[0].PropertyType.IsValueType)
-                    {
-                        gen.Eval(_ => _.Alloc(hashCode).As(_.Invoke(_.LdArg(0), _.X(gm)).GetHashCode()));
-                    }
-                    else
-                    {
-                        gen.Eval(_ => _.Alloc(hashCode).As(_.Invoke(_.LdArg(0), _.X(gm)) == null ? 
-                                                            0 : _.Invoke(_.LdArg(0), _.X(gm)).GetHashCode()));
+                        gen.Eval(_ => _.Alloc(hashCode).As(obj == null ? 0 : obj.GetHashCode()));
                     }
 
                     for (int i = 1; i < properties.Length; i++)
                     {
-                        gm = properties[i].GetGetMethod();
+                        gen.Eval(_ => _.Alloc(obj).As(properties[i].GetValue(_this, null)));
                         if (properties[i].PropertyType.IsValueType)
                         {
-                            gen.Eval(_ => _.Alloc(hashCode).As(hashCode ^ _.Invoke(_.LdArg(0), _.X(gm)).GetHashCode()));
+                            gen.Eval(_ => _.Alloc(hashCode).As(hashCode ^ obj.GetHashCode()));
                         }
                         else
                         {
-                            gen.Eval(_ => _.Alloc(hashCode).As(hashCode ^ (_.Invoke(_.LdArg(0), _.X(gm)) == null ? 
-                                                                            0 : _.Invoke(_.LdArg(0), _.X(gm)).GetHashCode())));
+                            gen.Eval(_ => _.Alloc(hashCode).As(hashCode ^ (obj == null ? 0 : obj.GetHashCode())));
                         }
                     }
-
-                    gen.Eval(_ => _.Return(hashCode));
                 }
+
+                gen.Eval(_ => _.Return(hashCode));
             });
             return (Func<T, int>)method.CreateDelegate(typeof(Func<T, int>));
         }
