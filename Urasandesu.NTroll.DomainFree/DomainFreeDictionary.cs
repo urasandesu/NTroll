595af1534b9a7e18a9854188445f5a0cb6992a7f
Akira Sugiura <urasandesu@gmail.com>
2012-01-21 19:01:55 +0900

    To Create Domain Free Instance Sample 01: Add Actual Sample For Domain Free Instantiation
    Add DomainFreeDictionary that can access to theirs data from other AppDomain. When I create this class, I used Mr. Einar's "Cross AppDomain Singleton" as a reference(see also he's blog, http://www.dolittle.com/blogs/einar/archive/2007/05/18/cross-appdomain-singleton.aspx).
    Add DomainFreeInstantiator that can instantiate any classes - they don't take to extend MarshalByRefObject or to set SerializableAttribute!! - from other AppDomain. This trick is used of the behavior that CLR doesn't check the type matching between AppDomains if there isn't a opcode castclass.

---
+++ Urasandesu.NTroll.DomainFree/DomainFreeDictionary.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using mscoree;
+
+namespace Urasandesu.NTroll.DomainFree
+{
+    public class DomainFreeDictionary<TKey, TValue> : MarshalByRefObject
+    {
+        static readonly string DictionaryName = "Domain Free Dictionary";
+        static DomainFreeDictionary<TKey, TValue> m_instance;
+        Dictionary<TKey, TValue> m_contents = new Dictionary<TKey, TValue>();
+
+        static AppDomain GetAppDomain(string friendlyName)
+        {
+            var hEnum = IntPtr.Zero;
+            var host = new CorRuntimeHostClass();
+            try
+            {
+                host.EnumDomains(out hEnum);
+
+                var pAppDomain = default(object);
+                while (true)
+                {
+                    host.NextDomain(hEnum, out pAppDomain);
+                    if (pAppDomain == null)
+                    {
+                        break;
+                    }
+                    var domain = (AppDomain)pAppDomain;
+                    if (domain.FriendlyName == friendlyName)
+                    {
+                        return domain;
+                    }
+                }
+            }
+            finally
+            {
+                host.CloseEnum(hEnum);
+                Marshal.ReleaseComObject(host);
+                host = null;
+            }
+            return null;
+        }
+
+
+        public static DomainFreeDictionary<TKey, TValue> Instance
+        {
+            get
+            {
+                if (m_instance == null)
+                {
+                    var domain = GetAppDomain(DictionaryName);
+                    if (domain == null)
+                    {
+                        domain = AppDomain.CreateDomain(DictionaryName);
+                    }
+                    var type = typeof(DomainFreeDictionary<TKey, TValue>);
+                    var instance = (DomainFreeDictionary<TKey, TValue>)domain.GetData(type.FullName);
+                    if (instance == null)
+                    {
+                        instance = (DomainFreeDictionary<TKey, TValue>)domain.CreateInstanceAndUnwrap(type.Assembly.FullName, type.FullName);
+                        domain.SetData(type.FullName, instance);
+                    }
+                    m_instance = instance;
+                }
+
+                return m_instance;
+            }
+        }
+
+        public void AddIfNotExist(TKey key, TValue value)
+        {
+            if (!m_contents.ContainsKey(key))
+            {
+                m_contents.Add(key, value);
+            }
+        }
+
+        public TValue GetIfExist(TKey key)
+        {
+            return m_contents.ContainsKey(key) ? m_contents[key] : default(TValue);
+        }
+
+        public void Clear()
+        {
+            m_contents.Clear();
+        }
+    }
+}
---
+++ Urasandesu.NTroll.DomainFree/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Urasandesu.NTroll.DomainFree
+{
+    public class MyCounter
+    {
+        int m_value;
+        public void Increment() { ++m_value; }
+        public int Value { get { return m_value; } }
+    }
+
+    public class MySingleton
+    {
+        MySingleton()
+        {
+            Counter = new MyCounter();
+        }
+        static MySingleton m_instance = new MySingleton();
+        public static MySingleton Instance { get { return m_instance; } }
+        public MyCounter Counter { get; private set; }
+    }
+
+    public class MarshalByRefRunner : MarshalByRefObject
+    {
+        public Action Action { get; set; }
+        public void Run()
+        {
+            if (Action != null)
+            {
+                Action();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AppDomain: {0}", AppDomain.CurrentDomain.FriendlyName);
+            Console.WriteLine("MySingleton.Instance: {0}", MySingleton.Instance.GetHashCode());
+            MySingleton.Instance.Counter.Increment();
+            Console.WriteLine("MySingleton.Instance.Counter.Value: {0}", MySingleton.Instance.Counter.Value);
+
+            DomainFreeInstantiator.Register(() => MySingleton.Instance);
+            Console.WriteLine("MySingleton.Instance: {0}", DomainFreeInstantiator.Get<MySingleton>().GetHashCode());
+            DomainFreeInstantiator.Get<MySingleton>().Counter.Increment();
+            Console.WriteLine("MySingleton.Instance.Counter.Value: {0}", DomainFreeInstantiator.Get<MySingleton>().Counter.Value);
+
+            var newDomain = AppDomain.CreateDomain("New Domain");
+            var runner = (MarshalByRefRunner)newDomain.CreateInstanceAndUnwrap(typeof(MarshalByRefRunner).Assembly.FullName, typeof(MarshalByRefRunner).FullName);
+            runner.Action = () =>
+            {
+                Console.WriteLine("AppDomain: {0}", AppDomain.CurrentDomain.FriendlyName);
+                Console.WriteLine("MySingleton.Instance: {0}", DomainFreeInstantiator.Get<MySingleton>().GetHashCode());
+                DomainFreeInstantiator.Get<MySingleton>().Counter.Increment();
+                Console.WriteLine("MySingleton.Instance.Counter.Value: {0}", DomainFreeInstantiator.Get<MySingleton>().Counter.Value);
+            };
+            runner.Run();
+        }
+    }
+}
