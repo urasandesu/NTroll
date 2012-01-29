105ff3e5582c4c7fc5dd1d038b0fd456749bbd86
Akira Sugiura <urasandesu@gmail.com>
2012-01-29 19:21:14 +0900

    To Create Domain Free Instance Sample 01: Modify Classes To Make Fair Copy
    Add an unit test ConfigurationManagerTest to describe the feature of AppDomain.

---
+++ Test.Urasandesu.NTroll.DomainFree/ConfigurationManagerTest.cs
@@ -0,0 +1,431 @@
+﻿//#define ADD_NEW_FEATURE
+//#define AFTER_FIX
+#define BEFORE_FIX
+
+
+#if _
+#elif ADD_NEW_FEATURE
+using System;
+using System.IO;
+using NUnit.Framework;
+using ThirdPartyLibrary;
+using Urasandesu.NTroll.DomainFree;
+
+namespace Test.Urasandesu.NTroll.DomainFree
+{
+    [TestFixture]
+    public class ConfigurationManagerTest
+    {
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+        }
+
+        [Test]
+        public void GetPropertyTestSuccess01ExistKeyExistValue()
+        {
+            var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+            Assert.AreEqual(DayOfWeek.Monday, holiday);
+        }
+
+        [Test]
+        public void GetPropertyTestError01AppConfigNotFound()
+        {
+            var info = new AppDomainSetup();
+            info.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            info.ShadowCopyFiles = AppDomain.CurrentDomain.SetupInformation.ShadowCopyFiles;
+            info.ConfigurationFile = Path.GetTempFileName();
+            File.Delete(info.ConfigurationFile);
+            AppDomain.CurrentDomain.RunAtIsolatedDomain(info, () =>
+            {
+                var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+                Assert.AreEqual(holiday, DayOfWeek.Sunday);
+            });
+        }
+
+        [Test]
+        [ExpectedException(typeof(System.Configuration.ConfigurationErrorsException))]
+        public void GetPropertyTestError02InvalidAppConfig()
+        {
+            var info = new AppDomainSetup();
+            info.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            info.ShadowCopyFiles = AppDomain.CurrentDomain.SetupInformation.ShadowCopyFiles;
+            info.ConfigurationFile = Path.GetTempFileName();
+            using (var sw = new StreamWriter(info.ConfigurationFile))
+            {
+                sw.Write("Hoge");
+            }
+            AppDomain.CurrentDomain.RunAtIsolatedDomain(info, () =>
+            {
+                var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+            });
+        }
+
+        [Test]
+        public void GetPropertyTestError03HolidayPropertyNotFound()
+        {
+            var info = new AppDomainSetup();
+            info.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            info.ShadowCopyFiles = AppDomain.CurrentDomain.SetupInformation.ShadowCopyFiles;
+            info.ConfigurationFile = Path.GetTempFileName();
+            using (var sw = new StreamWriter(info.ConfigurationFile))
+            {
+                sw.Write(@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<configuration>
+  <appSettings>
+  </appSettings>
+  <startup>
+    <supportedRuntime version=""v2.0.50727"" sku=""Client""/>
+  </startup>
+</configuration>");
+            }
+            AppDomain.CurrentDomain.RunAtIsolatedDomain(info, () =>
+            {
+                var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+                Assert.AreEqual(holiday, DayOfWeek.Sunday);
+            });
+        }
+
+        [Test]
+        public void GetPropertyTestError04HolidayPropertyIsEmpty()
+        {
+            var info = new AppDomainSetup();
+            info.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            info.ShadowCopyFiles = AppDomain.CurrentDomain.SetupInformation.ShadowCopyFiles;
+            info.ConfigurationFile = Path.GetTempFileName();
+            using (var sw = new StreamWriter(info.ConfigurationFile))
+            {
+                sw.Write(@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<configuration>
+  <appSettings>
+    <add key=""Holiday"" value="""" />
+  </appSettings>
+  <startup>
+    <supportedRuntime version=""v2.0.50727"" sku=""Client""/>
+  </startup>
+</configuration>");
+            }
+            AppDomain.CurrentDomain.RunAtIsolatedDomain(info, () =>
+            {
+                var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+                Assert.AreEqual(holiday, DayOfWeek.Sunday);
+            });
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetPropertyTestError05HolidayPropertyIsInvalidIfIgnoredCase()
+        {
+            var info = new AppDomainSetup();
+            info.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            info.ShadowCopyFiles = AppDomain.CurrentDomain.SetupInformation.ShadowCopyFiles;
+            info.ConfigurationFile = Path.GetTempFileName();
+            using (var sw = new StreamWriter(info.ConfigurationFile))
+            {
+                sw.Write(@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<configuration>
+  <appSettings>
+    <add key=""Holiday"" value=""aaaaaaaaaaaaa"" />
+  </appSettings>
+  <startup>
+    <supportedRuntime version=""v2.0.50727"" sku=""Client""/>
+  </startup>
+</configuration>");
+            }
+            AppDomain.CurrentDomain.RunAtIsolatedDomain(info, () =>
+            {
+                var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+            });
+        }
+
+        [Test]
+        public void GetPropertyTestError06HolidayPropertyIsInvalidIfCaseSensitive()
+        {
+            var info = new AppDomainSetup();
+            info.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            info.ShadowCopyFiles = AppDomain.CurrentDomain.SetupInformation.ShadowCopyFiles;
+            info.ConfigurationFile = Path.GetTempFileName();
+            using (var sw = new StreamWriter(info.ConfigurationFile))
+            {
+                sw.Write(@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<configuration>
+  <appSettings>
+    <add key=""Holiday"" value=""monday"" />
+  </appSettings>
+  <startup>
+    <supportedRuntime version=""v2.0.50727"" sku=""Client""/>
+  </startup>
+</configuration>");
+            }
+            AppDomain.CurrentDomain.RunAtIsolatedDomain(info, () =>
+            {
+                var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+            });
+        }
+    }
+}
+#elif AFTER_FIX
+using System;
+using System.IO;
+using NUnit.Framework;
+using ThirdPartyLibrary;
+using Urasandesu.NTroll.DomainFree;
+
+namespace Test.Urasandesu.NTroll.DomainFree
+{
+    [TestFixture]
+    public class ConfigurationManagerTest
+    {
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+        }
+
+        [Test]
+        public void GetPropertyTestSuccess01ExistKeyExistValue()
+        {
+            var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+            Assert.AreEqual(DayOfWeek.Monday, holiday);
+        }
+
+        [Test]
+        public void GetPropertyTestError01AppConfigNotFound()
+        {
+            var info = new AppDomainSetup();
+            info.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            info.ShadowCopyFiles = AppDomain.CurrentDomain.SetupInformation.ShadowCopyFiles;
+            info.ConfigurationFile = Path.GetTempFileName();
+            File.Delete(info.ConfigurationFile);
+            AppDomain.CurrentDomain.RunAtIsolatedDomain(info, () =>
+            {
+                var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+                Assert.AreEqual(holiday, DayOfWeek.Sunday);
+            });
+        }
+
+        [Test]
+        [ExpectedException(typeof(System.Configuration.ConfigurationErrorsException))]
+        public void GetPropertyTestError02InvalidAppConfig()
+        {
+            var info = new AppDomainSetup();
+            info.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            info.ShadowCopyFiles = AppDomain.CurrentDomain.SetupInformation.ShadowCopyFiles;
+            info.ConfigurationFile = Path.GetTempFileName();
+            using (var sw = new StreamWriter(info.ConfigurationFile))
+            {
+                sw.Write("Hoge");
+            }
+            AppDomain.CurrentDomain.RunAtIsolatedDomain(info, () =>
+            {
+                var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+            });
+        }
+
+        [Test]
+        public void GetPropertyTestError03HolidayPropertyNotFound()
+        {
+            var info = new AppDomainSetup();
+            info.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            info.ShadowCopyFiles = AppDomain.CurrentDomain.SetupInformation.ShadowCopyFiles;
+            info.ConfigurationFile = Path.GetTempFileName();
+            using (var sw = new StreamWriter(info.ConfigurationFile))
+            {
+                sw.Write(@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<configuration>
+  <appSettings>
+  </appSettings>
+  <startup>
+    <supportedRuntime version=""v2.0.50727"" sku=""Client""/>
+  </startup>
+</configuration>");
+            }
+            AppDomain.CurrentDomain.RunAtIsolatedDomain(info, () =>
+            {
+                var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+                Assert.AreEqual(holiday, DayOfWeek.Sunday);
+            });
+        }
+
+        [Test]
+        public void GetPropertyTestError04HolidayPropertyIsEmpty()
+        {
+            var info = new AppDomainSetup();
+            info.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            info.ShadowCopyFiles = AppDomain.CurrentDomain.SetupInformation.ShadowCopyFiles;
+            info.ConfigurationFile = Path.GetTempFileName();
+            using (var sw = new StreamWriter(info.ConfigurationFile))
+            {
+                sw.Write(@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<configuration>
+  <appSettings>
+    <add key=""Holiday"" value="""" />
+  </appSettings>
+  <startup>
+    <supportedRuntime version=""v2.0.50727"" sku=""Client""/>
+  </startup>
+</configuration>");
+            }
+            AppDomain.CurrentDomain.RunAtIsolatedDomain(info, () =>
+            {
+                var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+                Assert.AreEqual(holiday, DayOfWeek.Sunday);
+            });
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetPropertyTestError05HolidayPropertyIsInvalidIfIgnoredCase()
+        {
+            var info = new AppDomainSetup();
+            info.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            info.ShadowCopyFiles = AppDomain.CurrentDomain.SetupInformation.ShadowCopyFiles;
+            info.ConfigurationFile = Path.GetTempFileName();
+            using (var sw = new StreamWriter(info.ConfigurationFile))
+            {
+                sw.Write(@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<configuration>
+  <appSettings>
+    <add key=""Holiday"" value=""aaaaaaaaaaaaa"" />
+  </appSettings>
+  <startup>
+    <supportedRuntime version=""v2.0.50727"" sku=""Client""/>
+  </startup>
+</configuration>");
+            }
+            AppDomain.CurrentDomain.RunAtIsolatedDomain(info, () =>
+            {
+                var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+            });
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetPropertyTestError06HolidayPropertyIsInvalidIfCaseSensitive()
+        {
+            var info = new AppDomainSetup();
+            info.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            info.ShadowCopyFiles = AppDomain.CurrentDomain.SetupInformation.ShadowCopyFiles;
+            info.ConfigurationFile = Path.GetTempFileName();
+            using (var sw = new StreamWriter(info.ConfigurationFile))
+            {
+                sw.Write(@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<configuration>
+  <appSettings>
+    <add key=""Holiday"" value=""monday"" />
+  </appSettings>
+  <startup>
+    <supportedRuntime version=""v2.0.50727"" sku=""Client""/>
+  </startup>
+</configuration>");
+            }
+            AppDomain.CurrentDomain.RunAtIsolatedDomain(info, () =>
+            {
+                var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+            });
+        }
+    }
+}
+#elif BEFORE_FIX
+using System;
+using System.IO;
+using NUnit.Framework;
+using ThirdPartyLibrary;
+using Urasandesu.NTroll.DomainFree;
+
+namespace Test.Urasandesu.NTroll.DomainFree
+{
+    [TestFixture]
+    public class ConfigurationManagerTest
+    {
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+        }
+
+        [Test]
+        public void GetPropertyTestSuccess01ExistKeyExistValue()
+        {
+            var holiday = ConfigurationManager.GetProperty("Holiday", DayOfWeek.Sunday);
+            Assert.AreEqual(DayOfWeek.Monday, holiday);
+        }
+
+        [Test]
+        [Ignore("This test cannot be passed through. " +
+                "Though it needs modifying App.config, I could not do it " +
+                "because the configurations have already been cached " +
+                "when passing the success path.")]
+        public void GetPropertyTestError01AppConfigNotFound()
+        {
+            //
+        }
+
+        [Test]
+        [ExpectedException(typeof(System.Configuration.ConfigurationErrorsException))]
+        [Ignore("This test cannot be passed through. " +
+                "Though it needs modifying App.config, I could not do it " +
+                "because the configurations have already been cached " +
+                "when passing the success path.")]
+        public void GetPropertyTestError02InvalidAppConfig()
+        {
+            //
+        }
+
+        [Test]
+        [Ignore("This test cannot be passed through. " +
+                "Though it needs modifying App.config, I could not do it " +
+                "because the configurations have already been cached " +
+                "when passing the success path.")]
+        public void GetPropertyTestError03HolidayPropertyNotFound()
+        {
+            // 
+        }
+
+        [Test]
+        [Ignore("This test cannot be passed through. " +
+                "Though it needs modifying App.config, I could not do it " +
+                "because the configurations have already been cached " +
+                "when passing the success path.")]
+        public void GetPropertyTestError04HolidayPropertyIsEmpty()
+        {
+            // 
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        [Ignore("This test cannot be passed through. " +
+                "Though it needs modifying App.config, I could not do it " +
+                "because the configurations have already been cached " +
+                "when passing the success path.")]
+        public void GetPropertyTestError05HolidayPropertyIsInvalidIfIgnoredCase()
+        {
+            // 
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        [Ignore("This test cannot be passed through. " +
+                "Though it needs modifying App.config, I could not do it " +
+                "because the configurations have already been cached " +
+                "when passing the success path.")]
+        public void GetPropertyTestError06HolidayPropertyIsInvalidIfCaseSensitive()
+        {
+            // 
+        }
+    }
+}
+#endif
--- Urasandesu.NTroll.DomainFree/ConfigurationManager.cs
+++ Urasandesu.NTroll.DomainFree/ConfigurationManager.cs
@@ -1,4 +1,151 @@
-﻿using System;
+﻿//#define ADD_NEW_FEATURE
+
+#if ADD_NEW_FEATURE
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace ThirdPartyLibrary
+{
+    public class ConfigurationManager
+    {
+        public static T GetProperty<T>(string key, T defaultValue)
+        {
+            var value = global::System.Configuration.ConfigurationManager.AppSettings[key];
+            var impl = GetPropertyImpl<T>.CreateInstanceWithCache(GetPropertyImpl<T>.CreateInstance);
+            if (impl == null)
+                throw new NotSupportedException();
+            return impl.ToPropertyWithCache(key, value, defaultValue);
+        }
+
+        protected abstract class GetPropertyImpl<T>
+        {
+            static readonly object ms_lockObj = new object();
+            static GetPropertyImpl<T> ms_instance = null;
+            static bool ms_ready = false;
+
+            public static GetPropertyImpl<T> CreateSpecializedImplInstance<TSpecialized, TImpl>()
+            {
+                var t = typeof(T);
+                if (t == typeof(TSpecialized))
+                {
+                    var implType = typeof(TImpl);
+                    return Activator.CreateInstance(implType) as GetPropertyImpl<T>;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            public static GetPropertyImpl<T> CreateInstance()
+            {
+                var impl = default(GetPropertyImpl<T>);
+                if ((impl = CreateSpecializedImplInstance<DayOfWeek, GetPropertyImplForDayOfWeek>()) != null)
+                {
+                    return impl;
+                }
+                return impl;
+            }
+
+            public static GetPropertyImpl<T> CreateInstanceWithCache(Func<GetPropertyImpl<T>> instantiator)
+            {
+                if (!ms_ready)
+                {
+                    lock (ms_lockObj)
+                    {
+                        if (!ms_ready)
+                        {
+                            ms_instance = instantiator() as GetPropertyImpl<T>;
+                            Thread.MemoryBarrier();
+                            ms_ready = true;
+                        }
+                    }
+                }
+                return ms_instance;
+            }
+
+            public static void Reset()
+            {
+                if (ms_ready)
+                {
+                    lock (ms_lockObj)
+                    {
+                        if (ms_ready)
+                        {
+                            ms_instance = null;
+                            Thread.MemoryBarrier();
+                            ms_ready = false;
+                        }
+                    }
+                }
+            }
+
+            Dictionary<string, T> m_properties = new Dictionary<string, T>();
+            readonly object m_lockObj = new object();
+
+            public T ToPropertyWithCache(string key, string value, T defaultValue)
+            {
+                lock (m_lockObj)
+                {
+                    var property = default(T);
+                    if (m_properties.TryGetValue(key, out property))
+                    {
+                        return property;
+                    }
+                    else
+                    {
+                        property = ToPropertyCore(key, value, defaultValue);
+                        m_properties.Add(key, property);
+                        return property;
+                    }
+                }
+            }
+
+            protected abstract T ToPropertyCore(string key, string value, T defaultValue);
+        }
+
+        class GetPropertyImplForDayOfWeek : GetPropertyImpl<DayOfWeek>
+        {
+            const string DayOfWeekName = "DayOfWeek";
+            static readonly string DayOfWeekRegexPattern = 
+                                            string.Format(@"^\s*(?<{0}>" + 
+                                                                    @"(Sunday)|" + 
+                                                                    @"(Monday)|" + 
+                                                                    @"(Tuesday)|" + 
+                                                                    @"(Wednesday)|" + 
+                                                                    @"(Thursday)|" + 
+                                                                    @"(Friday)|" + 
+                                                                    @"(Saturday)" + 
+                                                              @")\s*$", DayOfWeekName);
+            static readonly Regex DayOfWeekRegex = new Regex(DayOfWeekRegexPattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            protected override DayOfWeek ToPropertyCore(string key, string value, DayOfWeek defaultValue)
+            {
+                var m = default(Match);
+                if (string.IsNullOrEmpty(value))
+                {
+                    return defaultValue;
+                }
+                else if ((m = DayOfWeekRegex.Match(value)) != null && m.Success)
+                {
+                    return (DayOfWeek)Enum.Parse(typeof(DayOfWeek), m.Groups[DayOfWeekName].Value, true);
+                }
+                else
+                {
+                    var fmt = "The value \"{0}\" linked by key \"{1}\" is invalid. " +
+                              "It must take one of the following values: \r\n{2}.";
+                    var msg = string.Format(fmt, value, key, string.Join(", ", Enum.GetNames(typeof(DayOfWeek))));
+                    throw new ArgumentException(msg, "value");
+                }
+            }
+        }
+
+        protected ConfigurationManager() { }
+    }
+}
+#else
+using System;
 using System.Collections.Generic;
 using System.Threading;
 
@@ -127,3 +274,4 @@ namespace ThirdPartyLibrary
         protected ConfigurationManager() { }
     }
 }
+#endif
